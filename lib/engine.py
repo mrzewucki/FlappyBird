dcb6300e2381a5d8bfadeb0813d28682d7f08080
Marcin Rzewucki <marcin@rzewucki.com>
2022-02-07 20:18:17 +0100

    Add engine module

---
+++ lib/engine.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Flappy Bird - engine module"""
+
+__author__ = "Marcin Rzewucki"
+__copyright__ = "Copyright 2022"
+__version__ = "0.0.1"
+__maintainer__ = "Marcin Rzewucki"
+__email__ = "marcin@rzewucki.com"
+__status__ = "Development"
+
+import pygame
